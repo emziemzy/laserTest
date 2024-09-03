5599b7ca948cc55284330661ac17bd017ca82511
emziemzy <emma.chapman@maxvms.com>
2024-09-04 09:53:15 +1000

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ def RunPoint(move: DobotApiMove, point_list: list,speedlparam):
         point_list = point_list.tolist()
     else:
         print("my_array is not a numpy array")
+        
     move.MovL(point_list[0], point_list[1], point_list[2], point_list[3], point_list[4], point_list[5],speedlparam)
 
 def RunCircle(move: DobotApiMove, point_list1: list, point_list2: list, count,speedlparam,acclparam):
@@ -44,7 +45,8 @@ def RunCircle(move: DobotApiMove, point_list1: list, point_list2: list, count,sp
         point_list = point_list.tolist()
     else:
         print("my_array is not a numpy array")
-   move.Circle3(point_list1[0], point_list1[1], point_list1[2], point_list1[3], point_list1[4], point_list1[5],point_list2[0], point_list2[1], point_list2[2], point_list2[3], point_list2[4], point_list2[5],count,speedlparam,acclparam)     
+        
+    move.Circle3(point_list1[0], point_list1[1], point_list1[2], point_list1[3], point_list1[4], point_list1[5],point_list2[0], point_list2[1], point_list2[2], point_list2[3], point_list2[4], point_list2[5],count,speedlparam,acclparam)     
 
 def MarkCircle(move: DobotApiMove, point_list1: list, point_list2: list, count,speedlparam,acclparam):
     if isinstance(point_list, np.ndarray):
@@ -52,7 +54,8 @@ def MarkCircle(move: DobotApiMove, point_list1: list, point_list2: list, count,s
         point_list = point_list.tolist()
     else:
         print("my_array is not a numpy array")
-   move.Circle3(point_list1[0], point_list1[1], point_list1[2], point_list1[3], point_list1[4], point_list1[5],point_list2[0], point_list2[1], point_list2[2], point_list2[3], point_list2[4], point_list2[5],count,speedlparam,acclparam)    
+        
+    move.Circle3(point_list1[0], point_list1[1], point_list1[2], point_list1[3], point_list1[4], point_list1[5],point_list2[0], point_list2[1], point_list2[2], point_list2[3], point_list2[4], point_list2[5],count,speedlparam,acclparam)    
 
 
 
