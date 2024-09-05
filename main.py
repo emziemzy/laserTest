a6b5919e3432fa1d06cdb83df7eb528e007ac52b
emziemzy <emma.chapman@maxvms.com>
2024-09-05 15:02:00 +1000

    Fixing false to False and true to True

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def WaitArrive(point_list):
         globalLockValue.release()
         sleep(0.001)
 
-def MarkTilArrive(point_list,continue=false):
+def MarkTilArrive(point_list,continue=False):
     print("MarkTilArrive function")
     if isinstance(point_list, np.ndarray):
         # print("my_array is a numpy array")
@@ -250,7 +250,7 @@ if __name__ == '__main__':
                 WaitArrive(point_c1)
                 
                 RunCircle(move, point_c2,point_c3,1,"SpeedL=1","AccL=1")
-                MarkTilArrive(point_c3,continue=true)
+                MarkTilArrive(point_c3,continue=True)
                 MarkTilArrive(point_c1)
                 #RunPoint(move, point_c1,"SpeedL=100")   
         
